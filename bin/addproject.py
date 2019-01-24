e5e2f5cf0c1beabc7e9784cb525cf917702cd5e2
penguin <leeminsuok@naver.com>
2019-01-24 11:51:37 +0900

    add addproject.py

---
+++ bin/addproject.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#coding:utf8
+import sys
+import os
+
+def addProject(name):
+	"""
+	프로젝트를 생성하는 함수입니다.
+	"""
+	root = "/project"
+	subDirs = ["shot", 
+	"in", 	
+	"out", 
+	"edit", 
+	"asset",  
+	"asset/char",
+	"asset/shader",
+	"config",
+	"config/ocio",
+	"config/thumbnail",
+	"doc",
+	"doc/concept"]
+
+	for s in subDirs:
+		p = "/".join([root,name,s])
+		os.makedirs(p)
+
+if __name__ == "__main__":
+	if  len(sys.argv) != 2:
+		print "How to use:"
+		print "$ addproject projectname"
+		sys.exit()
+	name = sys.argv[1]
+	addProject(name)
