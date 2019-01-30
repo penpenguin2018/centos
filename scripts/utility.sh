ba25a2bd59b1581d282dae6db0e4dc31d06f1bbb
penguin <leeminsuok@naver.com>
2019-01-30 15:17:06 +0900

    Imagemagick설치

--- scripts/utility.sh
+++ scripts/utility.sh
@@ -8,6 +8,7 @@ yum -y install transmission
 yum -y install tigervnc
 yum -y install filezilla
 yum -y install tree
+yum -y install ImageMagick
 
 # ripgrep
 yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
