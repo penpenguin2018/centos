9fc689d7c91af277c5336e7c6b6b3166181eb3e6
penguin <echo leeminsuok@naver.com>
2019-02-11 11:10:09 +0900

    Excel 파서

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -2,3 +2,10 @@
 cd /tmp
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
+pip install --user Cython
+pip install --user openpyxl
+pip install --user xlsxwriter
+pip install --user xlrd
+pip install --user xlwt
+pip install --user xlutils
+pip install --user pillow
