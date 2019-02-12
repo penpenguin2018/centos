86a1309e08540e9f9b38fbb8e635d3b66603e65d
penguin <echo leeminsuok@naver.com>
2019-02-12 12:15:10 +0900

    cmake, openex, opencolorio 추가

---
+++ scripts/opencolorio.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd ~/app
+git clone https://github.com/imageworks/OpenColorIO-Configs
+cd OpenColorIO_src
+git tag
+git checkout v1.0.9
+cd ..
+mkdir OpenColorIO_build
+cd OpenColorIO_build
+/home/$USER/app/cmake-3.13.3/bin/cmake ../OpenColorIO_src -DCMAKE_INSTALL_PREFIX=$HOME/app/OpenColorIO
+make
+make install
---
+++ scripts/openexr.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /home/$USER/app
+wget https://github.com/openexr/openexr/archive/v2.3.0.tar.gz
+tar -zxvf v2.3.0.tar.gz
+cd openexr-2.3.0
+cd IlmBase
+./bootstrap
+./configure --prefix=$HOME/app/openexr --with-ilmbase-prefix=$HOME/app/IlmBase
+make
+make install
